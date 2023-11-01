61c2f8451886f6eadf6497d70ff208260e8b6a82
RomanG <r.garanin@intensa.ru>
2023-11-01 15:51:55 +0300

    Refactor

--- storybook-static/assets/index-c7a2ee6b.js
+++
@@ -1,2 +0,0 @@
-import{M,a as D,u as X,w as C}from"./index-2423d116.js";import"./index-165335ed.js";import"./iframe-e62275c0.js";import"../sb-preview/runtime.mjs";import"./_commonjsHelpers-87174ba5.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-dd3358aa.js";import"./index-356e4a49.js";export{M as MDXContext,D as MDXProvider,X as useMDXComponents,C as withMDXComponents};
-//# sourceMappingURL=index-c7a2ee6b.js.map
